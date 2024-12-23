86690e7ed4859df9747397e1254c26919230f702
EKKop-eng <edgaras.kuprys123@gmail.com>
2024-12-24 01:45:14 +0200

    Update ApplicationDbContext.cs

--- DotNetCoreCRUDApp/Models/ApplicationDbContext.cs
+++ DotNetCoreCRUDApp/Models/ApplicationDbContext.cs
@@ -1,10 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-private readonly ApplicationDbContext _context;
-
-public HomeController(ApplicationDbContext context)
-{
-    _context = context;
-}
 
 namespace DotNetCoreApp.Models
 {
