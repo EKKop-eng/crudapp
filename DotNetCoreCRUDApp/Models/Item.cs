df77856c55127e97be686fa006456059f8a91cff
EKKop-eng <edgaras.kuprys123@gmail.com>
2024-12-24 11:02:57 +0200

    Update Item.cs

--- DotNetCoreCRUDApp/Models/Item.cs
+++ DotNetCoreCRUDApp/Models/Item.cs
@@ -1,22 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Items List</title>
-</head>
-<body>
-    <h1>Items List</h1>
-
-    <!-- Link to add a new item -->
-    <a href="/Home/Add">Add New Item</a>
-
-    <!-- List of items from the database -->
-    <ul>
-        <!-- Loop through the items and display them -->
-        <li>Item 1 <a href="/Home/Delete/1">Delete</a></li>
-        <li>Item 2 <a href="/Home/Delete/2">Delete</a></li>
-        <li>Item 3 <a href="/Home/Delete/3">Delete</a></li>
-    </ul>
-</body>
-</html>
+namespace MyApp.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
